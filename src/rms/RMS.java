e1bd2937a8f8e2276d178cc42dd0f04efedb6b1f
Soupaul <souparnopaul@gmail.com>
2020-09-12 03:52:31 +0530

    Login Form added

---
+++ src/rms/RMS.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rms;
+
+import rms.screens.Login;
+import javax.swing.*;
+
+/**
+ *
+ * @author soupa
+ */
+public class RMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable(){
+        
+            @Override
+            public void run(){
+            
+                Login loginForm = new Login();
+                loginForm.setVisible(true);
+                
+            }
+            
+        });
+        
+    }
+    
+}
